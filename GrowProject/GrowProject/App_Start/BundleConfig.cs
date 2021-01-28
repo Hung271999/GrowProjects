5a9e61990033682aa368db5a1cb6fa41c82d3dd0
hung271999@gmail.com <hung271999@gmail.com>
2021-01-28 15:15:28 +0700

    add

--- GrowProject/GrowProject/App_Start/BundleConfig.cs
+++ GrowProject/GrowProject/App_Start/BundleConfig.cs
@@ -9,6 +9,22 @@ namespace GrowProject
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                    //  < !--ALL JS FILES-- >//
+                        "~/js/jquery-3.2.1.min.js",
+                        "~/js/popper.min.js",
+                        // <!-- ALL PLUGINS --> //
+                        "~/js/bootstrap.min.js",
+                        "~/js/jquery.superslides.min.js",
+                        "~/js/bootstrap-select.js",
+                        "~/js/inewsticker.js",
+                        "~/js/bootsnav.js",
+                        "~/js/images-loded.min.js",
+                        "~/js/isotope.min.js",
+                        "~/js/owl.carousel.min.js",
+                        "~/js/baguetteBox.min.js",
+                        "~/js/form-validator.min.js",
+                        "~/js/contact-form-script.js",
+                        "~/js/custom.js",
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
@@ -20,11 +36,16 @@ namespace GrowProject
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                          "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+                 bundles.Add(new StyleBundle("~/Contents/css").Include(
+                      "~/css/bootstrap.min.css",
+                      "~/css/style.css",
+                       "~/css/responsive.css",
+                       "~/css/custom.css"));
         }
     }
 }
